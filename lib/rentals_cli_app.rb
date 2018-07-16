6ac8ad6bfb216f64dea4139c4a0b67e0634f8e86
github username <github email address>
2018-07-15 20:56:20 -0400

    Update 1
    Base file.

--- lib/rentals_cli_app.rb
+++ lib/rentals_cli_app.rb
@@ -1,4 +1,7 @@
-require "rentals_cli_app/version"
+require_relative "rentals_cli_app/version"
+require_relative './rentals_cli_app/cli.rb'
+require_relative './rentals_cli_app/rentals.rb'
+require_relative './rentals_cli_app/scraper.rb'
 
 module RentalsCliApp
   # Your code goes here...
---
+++ lib/rentals_cli_app/cli.rb
@@ -0,0 +1,6 @@
+class Rentalscli::CLI
+
+def call
+  puts "This is working!!!!"
+end #end of class
+end #end of call
